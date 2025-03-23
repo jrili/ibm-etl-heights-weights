59e7782ac8dbbf367abcdc1cb4d1075d6b00182a
jrili <jfmrili@gmail.com>
2025-03-23 21:47:34 +0800

    etl_code.py: refactor dataframe concatenations (append extracted dataframes to list then concatenate all at once instead of concatenating after each result); use constants for conversion factors; capitalize constants;

--- etl_code.py
+++ etl_code.py
@@ -3,8 +3,8 @@ import pandas as pd
 import xml.etree.ElementTree as ET 
 from datetime import datetime 
 
-log_file = "log_file.txt"
-target_file = "transformed_data.csv"
+LOG_FILE = "log_file.txt"
+TARGET_OUTPUT_FILE = "transformed_data.csv"
 
 ############ Logging Methods ############
 def log(msg):
@@ -12,7 +12,7 @@ def log(msg):
     now = datetime.now()
     timestamp_str = now.strftime(timestamp_format)
 
-    with open(log_file, "a") as f:
+    with open(LOG_FILE, "a") as f:
         log_str = timestamp_str + "," + msg + "\n"
         f.write(log_str)
 
@@ -31,7 +31,7 @@ def extract_from_json(file_to_process):
 
 def extract_from_xml(file_to_process):
     log(f"In extract_from_xml(): Extracting from file '{file_to_process}'")
-    dataframe = pd.DataFrame(columns=["name", "height", "weight"])
+    rows_list = []
     tree = ET.parse(file_to_process)
     root = tree.getroot()
     for person in root:
@@ -39,59 +39,37 @@ def extract_from_xml(file_to_process):
         height = float(person.find("height").text)
         weight = float(person.find("weight").text)
 
-        current_dataframe = pd.DataFrame([{"name":name, "height":height, "weight":weight}])
+        rows_list.append([{"name":name, "height":height, "weight":weight}])
 
-        # To avoid FutureWarning, avoid using concat() with an empty dataframe
-        if dataframe.empty:
-            dataframe = current_dataframe.copy()
-        else:
-            dataframe = pd.concat([dataframe, current_dataframe])
-
-    return dataframe
+    return pd.DataFrame(rows_list)
 
 def extract():
     log("In extract(): started")
-    # Create empty data frame with the corresponding headers
-    extracted_data = pd.DataFrame(columns=["name", "height", "weight"])
+    # Initialize list of extracted dataframes
+    # to be populated as the script goes through the dataset files
+    extracted_dfs_list = []
 
     # Process all CSV files
     log("In extract(): start processing CSV files")
     for csvfile in glob.glob("source/*.csv"):
-        current_dataframe = extract_from_csv(csvfile)
-
-        # To avoid FutureWarning, avoid using concat() with an empty dataframe
-        if extracted_data.empty:
-            extracted_data = current_dataframe.copy()
-        else:
-            extracted_data = pd.concat([extracted_data, current_dataframe],
-                ignore_index=True)
+        extracted_dfs_list.append(extract_from_csv(csvfile))
     log("In extract(): done processing CSV files")
 
     # Process all JSON files
     log("In extract(): start processing JSON files")
     for jsonfile in glob.glob("source/*.json"):
-        current_dataframe = extract_from_json(jsonfile)
-
-        # To avoid FutureWarning, avoid using concat() with an empty dataframe
-        if extracted_data.empty:
-            extracted_data = current_dataframe.copy()
-        else:
-            extracted_data = pd.concat([extracted_data, current_dataframe],
-                ignore_index=True)
+        extracted_dfs_list.append(extract_from_json(jsonfile))
     log("In extract(): done processing JSON files")
 
     # Process all XML files
     log("In extract(): start processing XML files")
     for xmlfile in glob.glob("source/*.xml"):
-        current_dataframe = extract_from_xml(xmlfile)
-
-        # To avoid FutureWarning, avoid using concat() with an empty dataframe
-        if extracted_data.empty:
-            extracted_data = current_dataframe.copy()
-        else:
-            extracted_data = pd.concat([extracted_data, current_dataframe],
-                ignore_index=True)
+        extracted_dfs_list.append(extract_from_xml(xmlfile))
     log("In extract(): done processing XML files")
+
+    # Concatenate all dataframes in the extracted_dfs_list
+    # into a single DataFrame
+    extracted_data = pd.concat(extracted_dfs_list)
 
     log("In extract(): ended")
     return extracted_data
@@ -100,12 +78,12 @@ def extract():
 def transform(data):
     log("In transform(): started")
     # Convert all Height values from in. to m
-    #   NOTE: 1 in. = 0.0254 m
-    data['height'] = round(data.height*0.0254, 2)
+    METERS_PER_INCH = 0.0254
+    data['height'] = round(data.height*METERS_PER_INCH, 2)
 
     # Convert all Weight values from lb to kg
-    #   NOTE: 1 lb =  0.45359237 kg
-    data['weight'] = round(data.weight*0.45359237, 2)
+    KG_PER_LB = 0.45359237
+    data['weight'] = round(data.weight*KG_PER_LB, 2)
 
     log("In transform(): ended")
     return data
@@ -118,7 +96,6 @@ def load_data(target_file, transformed_data):
     log(f"In load_data(): Done loading data to file '{target_file}'")
 
 
-
 ############ Main ############
 
 log("ETL Job Started")
@@ -126,7 +103,7 @@ extracted_data = extract()
 
 transformed_data = transform(extracted_data)
 
-load_data(target_file, transformed_data)
+load_data(TARGET_OUTPUT_FILE, transformed_data)
 
 log("ETL Job Ended\n")
 
